4c365bc30ea5a0d7646088e0ee9a3e488385f924
Pavel Shelkovenko <shelpikk02@gmail.com>
2023-06-16 00:23:56 +0300

    impl sharedRes for ios part

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -5,7 +5,9 @@ struct ContentView: View {
 	let greet = Greeting().greet()
 
 	var body: some View {
-		Text(greet)
+        VStack {
+            Text(Strings().get(id: SharedRes.strings().welcome, args: []))
+        }
 	}
 }
 
